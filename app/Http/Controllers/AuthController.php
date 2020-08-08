ffcef4109d2f1dd29d9d6e76ee3bcabac8f9c2e5
Ajeng Ruswanti <ajengruswanti1@gmail.com>
2020-08-08 14:30:16 +0700

    instalasi

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me(){
+        return "NIS : 3103118006
+                Nama : Ajeng Ruswanti
+                Gender : Perempuan
+                Phone : 088216793001
+                Kelas : XII RPL 1";
+    }
+}
